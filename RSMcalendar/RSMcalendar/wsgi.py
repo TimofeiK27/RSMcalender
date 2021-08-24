e96cd91509b2afba980b0ba2fedeb46aab1b3c1f
Timofei Kachan <67656268+TimofeiK27@users.noreply.github.com>
2021-08-24 15:18:34 -0700

    second commit

--- RSMcalendar/RSMcalendar/wsgi.py
+++ RSMcalendar/RSMcalendar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RSMcalender project.
+WSGI config for RSMcalendar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RSMcalender.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RSMcalendar.settings')
 
 application = get_wsgi_application()
